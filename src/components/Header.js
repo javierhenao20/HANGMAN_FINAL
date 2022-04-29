a522f35e6a49340b50e5b43baf8f28aebf06531a
Javier <javier.henao@uao.edu.co>
2022-04-29 15:05:26 -0500

    Hangman primer cambio

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <>
+    <h1>Hangman</h1>
+    <p>Encuentra la palabra oculta - Ingresa una letra</p>
+      
+    </>
+  );
+}
+
+export default Header;
